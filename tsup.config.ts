0eac5a73b573962d04a668f80bb801b351b484bf
yingpengsha <yingpengsha@kuaishou.com>
2022-12-08 20:52:47 +0800

    build: clean dir before build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig(({ env }) => {
   return {
     dts: true,
+    clean: true,
     outDir: 'dist',
     splitting: false,
     format: ['esm', 'cjs'],
